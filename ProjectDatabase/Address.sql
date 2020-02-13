9b8b3fd4ca899ba27626573931a985d7ee28155c
Adrian Sarwiński <sarwinski.adrian@gmail.com>
2020-02-14 00:45:48 +0100

    Created Database

---
+++ ProjectDatabase/Address.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Address]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [PersonId] INT NOT NULL, 
+    [City] NVARCHAR(50) NOT NULL, 
+    CONSTRAINT [FK_Address_ToTable] FOREIGN KEY (PersonId) REFERENCES [Person]([Id])
+
+)
---
+++ ProjectDatabase/Person.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Person]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Name] NVARCHAR(50) NOT NULL, 
+    [LastName] NVARCHAR(50) NOT NULL
+)
